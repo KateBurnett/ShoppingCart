7cd1224df47feb3820f0b0159dc51f2956a18995
Kate Burnett <119454443+KateBurnett@users.noreply.github.com>
2023-03-09 22:53:28 -0500

    Update fetch and added comments about errors

--- cart.jsx
+++ cart.jsx
@@ -10,68 +10,37 @@ const Cart = (props) => {
   const { Card, Accordion, Button } = ReactBootstrap;
   let data = props.location.data ? props.location.data : products;
   console.log(`data:${JSON.stringify(data)}`);
+  console.log("*************INSIDE CART******************");
+  // neither of these showing up in console ^
 
   return <Accordion defaultActiveKey="0">{list}</Accordion>;
 };
 
-const useDataApi = (initialUrl, initialData) => {
-  const { useState, useEffect, useReducer } = React;
-  const [url, setUrl] = useState(initialUrl);
+const useDataApi = () => {
+  const { useState, useEffect } = React;
+  const [data, setData] = useState();
+  const [url, setUrl] = useState("http://localhost:1337/api/products");
+  const [isLoading, setIsLoading] = useState(false);
 
-  const [state, dispatch] = useReducer(dataFetchReducer, {
-    isLoading: false,
-    isError: false,
-    data: initialData,
-  });
-  console.log(`useDataApi called`);
+  console.log("Rendering App");
+
   useEffect(() => {
-    console.log("useEffect Called");
-    let didCancel = false;
+    console.log("Fetching data...");
     const fetchData = async () => {
-      dispatch({ type: "FETCH_INIT" });
-      try {
-        const result = await axios(url);
-        console.log("FETCH FROM URl");
-        if (!didCancel) {
-          dispatch({ type: "FETCH_SUCCESS", payload: result.data });
-        }
-      } catch (error) {
-        if (!didCancel) {
-          dispatch({ type: "FETCH_FAILURE" });
-        }
-      }
+      setIsLoading(true);
+      const result = await axios(url);
+      console.log("**********************")
+      console.log(result.data.data[0])
+      // this^ works but data is still burried in another obj, adding .data and .attributes didn't work
+      console.log("*********************")
+      setData(result.data.data);
+      setIsLoading(false);
     };
+
     fetchData();
-    return () => {
-      didCancel = true;
-    };
-  }, [url]);
-  return [state, setUrl];
-};
-const dataFetchReducer = (state, action) => {
-  switch (action.type) {
-    case "FETCH_INIT":
-      return {
-        ...state,
-        isLoading: true,
-        isError: false,
-      };
-    case "FETCH_SUCCESS":
-      return {
-        ...state,
-        isLoading: false,
-        isError: false,
-        data: action.payload,
-      };
-    case "FETCH_FAILURE":
-      return {
-        ...state,
-        isLoading: false,
-        isError: true,
-      };
-    default:
-      throw new Error();
-  }
+  }, []);
+
+  return [setUrl];
 };
 
 const Products = (props) => {
@@ -88,17 +57,18 @@ const Products = (props) => {
     Image,
     Input,
   } = ReactBootstrap;
+
   //  Fetch Data
-  const { Fragment, useState, useEffect, useReducer } = React;
+  const { Fragment, useState, useEffect} = React;
   const [query, setQuery] = useState("http://localhost:1337/api/products");
-  const [{ data, isLoading, isError }, doFetch] = useDataApi(
+  const [{ data, isLoading }, doFetch] = useDataApi(
     "http://localhost:1337/api/products",
     {
       data: [],
     }
   );
   console.log(`Rendering Products ${JSON.stringify(data)}`);
-  // Fetch Data
+  // Fetch Data - no it doesn't, it renders the products variable
   const addToCart = (e) => {
     let name = e.target.name;
     let item = items.filter((item) => item.name == name);
@@ -106,9 +76,8 @@ const Products = (props) => {
     item[0].instock = item[0].instock - 1;
     console.log(`add to Cart ${JSON.stringify(item)}`);
     setCart([...cart, ...item]);
-    //doFetch(query);
-
   };
+
   const deleteCartItem = (delIndex) => {
     let newCart = cart.filter((item, i) => delIndex != i);
     let target = cart.filter((item, index) => delIndex == index)
@@ -119,7 +88,6 @@ const Products = (props) => {
     setCart(newCart);
     setItems(newItems);
   };
-  //const photos = ["apple.png", "orange.png", "beans.png", "cabbage.png"];
 
   let list = items.map((item, index) => {
     let n = index + 10;
@@ -168,17 +136,19 @@ const Products = (props) => {
     console.log(`total updated to ${newTotal}`);
     return newTotal;
   };
+
   //TODO: implement the restockProducts function
   const restockProducts = (url) => {
-  console.log("YYYYYYYYYYY")
-    console.log(doFetch(url));
+    let data = doFetch(url);
+    // now the error is "doFetch is not a function" but we don't see the same error in the old code which also only uses doFetch in this function and in data/isLoading set state
+    console.log("YYYYYYYYYYY");
+    console.log(url); // works if we remove doFetch
     console.log("YYYYYYYYYYY")
-    let newItems = data.data.map((item) => {
+    let newItems = data.data.map((item) => { //Error = "Cannot read properties of undefined (reading 'data')" which is why I tried to define data above
       let { name, country, cost, instock } = item;
       return { name, country, cost, instock }
     });
     console.log("@@@@@@@@@@@")
-    console.log(items)
     console.log(newItems)
     console.log("@@@@@@@@@@@")
     setItems([...items, ...newItems]);
